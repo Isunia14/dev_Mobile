acb77dc028887c8747f9b26e4d8e83e3fc5db02a
Louan Ourvouai <louan.ourvouai@farsight-vr.com>
2023-12-01 15:58:09 +0100

    Etape 10

--- TodoManonPoulain/app/src/main/java/com/manonpoulain/todo/list/TaskListFragment.kt
+++ TodoManonPoulain/app/src/main/java/com/manonpoulain/todo/list/TaskListFragment.kt
@@ -5,18 +5,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.manonpoulain.todo.R
 
 class TaskListFragment : Fragment() {
 
     private var taskList = listOf("Task 1", "Task 2", "Task 3")
+
+    private val adapter = TaskListAdapter()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val rootView = inflater.inflate(R.layout.fragment_task_list, container, false)
-
+        adapter.currentList = taskList
         return rootView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val recyclerView = view.findViewById<RecyclerView>(R.id.manonpoulain)
+        super.onViewCreated(view, savedInstanceState)
     }
 }
---
+++ TodoManonPoulain/app/src/main/java/com/manonpoulain/todo/list/TaskViewHolder.kt
@@ -0,0 +1,4 @@
+package com.manonpoulain.todo.list
+
+class TaskViewHolder {
+}
