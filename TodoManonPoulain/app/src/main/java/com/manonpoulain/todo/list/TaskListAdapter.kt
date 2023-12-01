ff70f13cb7759279ea79fc2ddd77ad5d9896e585
Louan Ourvouai <louan.ourvouai@farsight-vr.com>
2023-12-01 16:30:01 +0100

    Middle étpae 11

--- TodoManonPoulain/app/src/main/java/com/manonpoulain/todo/list/TaskListAdapter.kt
+++ TodoManonPoulain/app/src/main/java/com/manonpoulain/todo/list/TaskListAdapter.kt
@@ -1,7 +1,11 @@
 package com.manonpoulain.todo.list
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.manonpoulain.todo.R
 
 class TaskListAdapter : RecyclerView.Adapter<TaskListAdapter.TaskViewHolder>() {
 
@@ -11,8 +15,27 @@ class TaskListAdapter : RecyclerView.Adapter<TaskListAdapter.TaskViewHolder>() {
     inner class TaskViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         fun bind(taskTitle: String) {
             // on affichera les données ici
+
+            currentList.plus(taskTitle);
+
         }
     }
 
-    
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_task, parent, false)
+        return TaskViewHolder(itemView);
+    }
+
+    override fun getItemCount(): Int {
+        return currentList.count();
+    }
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        TODO("Not yet implemented")
+        TaskViewHolder.textRef
+
+
+    }
+
+
 }
--- TodoManonPoulain/app/src/main/java/com/manonpoulain/todo/list/TaskViewHolder.kt
+++ TodoManonPoulain/app/src/main/java/com/manonpoulain/todo/list/TaskViewHolder.kt
@@ -1,4 +1,12 @@
 package com.manonpoulain.todo.list
 
+import android.widget.TextView
+import com.manonpoulain.todo.R
+
 class TaskViewHolder {
+
+    //ToDo TextView
+
+
+    var textRef: TextView = (TextView) findViewById(R.id.task_title);
 }
